cf0b9e9ee2f9f19771d3ad27e4ede0223f747896
Troels Thomsen <troels@thomsen.io>
2013-06-20 14:01:02 +0200

    Extract module for running a remote command

---
+++ lib/heroku/command/remote_command.rb
@@ -0,0 +1,26 @@
+require 'base64'
+require 'em-eventsource'
+
+require_relative '../../hash'
+
+module Heroku::Command::RemoteCommand
+  def run_remote(command, params={})
+    EM.run do
+      params.merge!({ 'app' => app })
+
+      source_url = "https://#{Heroku::Auth.user}:#{Heroku::Auth.password}@heroku-repo-backend.herokuapp.com/commands/#{command}?#{params.to_query_string}"
+
+      source = EventMachine::EventSource.new(source_url)
+
+      source.on 'out' do |message| STDOUT << Base64.decode64(message) end
+      source.on 'err' do |message| STDERR << Base64.decode64(message) end
+
+      source.on 'close' do
+        source.close
+        EM.stop
+      end
+
+      source.start
+    end
+  end
+end
--- lib/heroku/command/repo.rb
+++ lib/heroku/command/repo.rb
@@ -1,11 +1,10 @@
-require 'base64'
-require 'em-eventsource'
 require 'vendor/heroku/okjson'
 
-require_relative '../../hash'
+require_relative './remote_command'
 
 # Slug manipulation
 class Heroku::Command::Repo < Heroku::Command::BaseWithApp
+  include Heroku::Command::RemoteCommand
 
   # repo:purge_cache
   #
@@ -54,25 +53,5 @@ class Heroku::Command::Repo < Heroku::Command::BaseWithApp
 
   def repo_get_url
     release['repo_get_url']
-  end
-
-  def run_remote(command, params={})
-    EM.run do
-      params.merge!({ 'app' => app })
-
-      source_url = "https://#{Heroku::Auth.user}:#{Heroku::Auth.password}@heroku-repo-backend.herokuapp.com/commands/#{command}?#{params.to_query_string}"
-
-      source = EventMachine::EventSource.new(source_url)
-
-      source.on 'out' do |message| STDOUT << Base64.decode64(message) end
-      source.on 'err' do |message| STDERR << Base64.decode64(message) end
-
-      source.on 'close' do
-        source.close
-        EM.stop
-      end
-
-      source.start
-    end
   end
 end
